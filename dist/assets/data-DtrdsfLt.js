3438f51763698e86fe7a87cd6138ea13fcd6e9b8
defve <103113785+chuanlongZhou@users.noreply.github.com>
2024-08-27 10:25:14 +0200

    update team member and teams

--- dist/assets/data-DtrdsfLt.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./Hero-Z6Isprw9.js";import{_ as t,z as e,A as s}from"./index-B-nhMJWM.js";const _={};function a(c,r){const n=o;return e(),s(n,{main:"this is data page",sub:"not finished"})}const f=t(_,[["render",a]]);export{f as default};
--- dist/assets/methodology-CnjbLL7D.js
+++
@@ -1,1 +0,0 @@
-import{_}from"./Hero-Z6Isprw9.js";import{z as e,A as t}from"./index-B-nhMJWM.js";const a={__name:"methodology",setup(n){return(r,c)=>{const o=_;return e(),t(o)}}};export{a as default};
